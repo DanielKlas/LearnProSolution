5c185949658a9fcf81d657916b8b664daec9af57
Daniel <danielklas@outlook.com>
2023-06-04 21:46:28 +0100

    database connection inserted

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+//Set dependencies
 const express = require('express');
+const app = express();
 const mysql = require('mysql');
+const mime = require('mime');
+var path = require('path');
 require('dotenv').config();
 
-const app = express();
-app.use(express.static(__dirname + '/public'));
+//Set up static file access
+app.use(express.static('public', {
+  setHeaders: (res, path) => {
+    res.setHeader('Content-Type', mime.getType(path));
+  }
+}));
 
+//Set up views
+app.set('views', './views')
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.render('index');
 });
+
+app.use('/scripts', (req, res, next) => {
+  res.setHeader('Content-Type', 'text/javascript');
+  next();
+});
+
+app.use(express.static('public'));
 
 // Custom port for Heroku, cannot be changed
 let port = process.env.PORT;
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,13 @@
 var table = document.getElementById('data-body');
 
 table.addEventListener('click', function(event) {
+  console.log('Table cell clicked');
   var target = event.target;
 
-  if (target.tagName === 'TD' && !target.hasAttribute('data-clicked')) {
+  if (target.tagName === 'TH' && !target.hasAttribute('data-clicked')) {
     target.setAttribute('data-clicked', 'yes');
     target.setAttribute('data-text', target.innerHTML);
-
+    
     var input = document.createElement('input');
     input.setAttribute('type', 'text');
     input.value = target.innerHTML;
@@ -24,8 +25,6 @@ table.addEventListener('click', function(event) {
       var current_text = this.value;
 
       if (orig_text !== current_text) {
-        // need to add regex and database call here
-        // must be a single database call as multiple will affect performance when opening and closing connection
         td.innerHTML = current_text;
         console.log(orig_text + ' now changed to ' + current_text);
       } else {
